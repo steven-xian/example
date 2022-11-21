782c2300ea64452a3c8d1a7514d0f7a7fab0e0e8
steven <xianxin188@gmail.com>
2022-11-21 17:52:37 +0800

    update

---
+++ learn_python/series.py
@@ -0,0 +1,18 @@
+
+def series_sum(start1, start2, end1, end2, step):
+    mul1 = start1
+    mul2 = start2
+    num = []
+    for idx in range(int((end1 - start1) / step) + 1):
+        print("num:{} * {}".format(mul1, mul2))
+        num.append(mul1 * mul2)
+        mul1 += step
+        mul2 += step
+
+    print(num) 
+    print("sum:", sum(num))
+
+if __name__ == "__main__":
+    series_sum(11, 13, 97, 99, 2)
+
+    
