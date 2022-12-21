1087b651180c4f1900c4a8f1ec179c7db64b69cc
steven <xianxin188@gmail.com>
2022-12-21 13:49:03 +0800

    update

--- learn_qt/file_demo/mainwindow.cpp
+++ learn_qt/file_demo/mainwindow.cpp
@@ -1,5 +1,8 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +16,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_write_clicked()
+{
+    QFile file("file.dat");
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate))
+    {
+        qDebug() << "failed to write data";
+        return;
+    }
+
+    QByteArray data;
+    data.clear();
+    data.push_back("DPPS");
+    data.push_back(quint8(1));
+    data.push_back(quint8(3));
+    file.write(data, data.size());
+
+    qDebug() << "sizeof time_t:" << sizeof(time_t);
+    qDebug() << "data.size:" << data.size();
+//    QDataStream out(&file);
+//    out.setVersion(QDataStream::Ok);
+
+//    out << "DPPS" << quint8(1) << quint8(3);
+}
